07289d7632dc0732ce8834936a7fa2e3e9c22314
Smitha Konduri <smitha.konduri@gmail.com>
2021-01-22 21:14:36 +0000

    adding remote backend

--- variables.tf
+++ variables.tf
@@ -6,6 +6,7 @@
 # prompted to enter it (or define it via config file or command line flags.)
 
 variable "prefix" {
+  
   description = "This prefix will be included in the name of most resources."
 }
 
@@ -48,3 +49,4 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
